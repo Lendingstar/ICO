7c6135ca03f402deb4c7052dd8b344c1d7932a6b
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2017-11-01 13:10:27 +0200

    Bonus functionality for LSMultiSale contract

---
+++ contracts/LSMultiSaleWithBonus.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.4.15;
+
+
+import './LSMultiSale.sol';
+
+
+/**
+* @title Lending Star Crowdsale
+* @author LendingStar Malaysia Sdn Bhd.
+* 
+* Lending Star crowdsale smart contract with bonuses
+*/
+contract LSMultiSaleWithBonus is LSMultiSale
+{
+    using SafeMath for uint256;
+
+    function LSMultiSaleWithBonus(
+        uint256 _rate,
+        uint256 _goal,
+        uint256 _weiMinimalInvestment,
+        uint256 _startTime,
+        uint256 _endTime,
+        address _tokenAddress,
+        address _wallet
+    ) public
+        LSMultiSale(
+            _rate,
+            _goal,
+            _weiMinimalInvestment,
+            _startTime,
+            _endTime,
+            _tokenAddress,
+            _wallet
+        )
+    {
+    }
+
+    /**
+    * @dev calculates amount of tokens that can be baught for _investments
+    * @param _investments uint256 represents amount of investments in Wei
+    * @return amount of tokens that can be bought for _investments
+    */
+    function weiToTokens(uint256 _investments) internal constant
+        returns (uint256)
+    {
+        // Follow bonus calculation logic for transactions larger than
+        // particular amount
+        if (_investments >= 100 ether) {
+            uint256 _bonus_percent = 0;
+
+            if (_investments < 300 ether) {
+                _bonus_percent = 15;
+            } else if (_investments >= 300 ether && _investments < 500 ether) {
+                _bonus_percent = 20;
+            } else if (_investments >= 500 ether && _investments < 700 ether) {
+                _bonus_percent = 25;
+            } else { // if _investments >= 700 ether
+                _bonus_percent = 30;
+            }
+
+            // Add bonuses (in Wei) to the origianl _investments:
+            // If original _investments - 100%
+            // recalculate how much Wei would (100 + _bonus_percent)% be
+            _investments = _investments.mul(100 + _bonus_percent).div(100);
+        }
+
+        // Calculate amount of tokens, using base class formula
+        return super.weiToTokens(_investments);
+    }
+}
